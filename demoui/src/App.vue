e9ef03d4cfc0f12015ad4b1165db579f3a9f7fc8
unknown <mario.lenicky@gmail.com>
2024-02-21 17:04:36 +0100

    generated sources removed

--- demoui/src/App.vue
+++ demoui/src/App.vue
@@ -18,15 +18,12 @@ export default {
     loaded: false,
     chartData: null
   }),
-  async mounted () {
+  async mounted() {
     this.loaded = false
 
     try {
-      const response = await axios.get('http://localhost:8080/api/v1/tempPast10Days') 
-      console.log(response.data)
-
+      const response = await axios.get('http://localhost:8080/api/v1/tempPast10Days')
       this.chartData = response.data
-
       this.loaded = true
     } catch (e) {
       console.error(e)
@@ -34,53 +31,3 @@ export default {
   }
 }
 </script>
-
-<!-- Generated Hello World App -->
-
-<!-- <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
-</script>
-
-<template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
-</template>
-
-<style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
-</style> -->
